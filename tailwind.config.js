7889b4cb76642484ae38e3af96bd9e8c86e367d5
Rao <jiahungrao@gmail.com>
2023-08-27 23:12:54 +0800

    setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hdprimary: {
+          DEFAULT: "#264710",
+        },
+        hdsecondary: {
+          DEFAULT: "#de9e36",
+        },
+        hdcontrast: {
+          DEFAULT: "#800000",
+        },
+      },
+    },
   },
   plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      $src: resolve('./src'),
+      $assets: resolve('./src/assets'),
+      $containers: resolve('./src/containers'),
+      $components: resolve('./src/components'),
+      $pages: resolve('./src/pages'),
+      $stores: resolve('./src/stores'),
+      $types: resolve('./src/types'),
+      $api: resolve('./src/api'),
+    }
+  },
 })
